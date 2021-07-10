60cc53268445898ce7ea2c7152a115578a8be7b8
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-10 01:54:29 -0500

    Delete seeds.sql

--- seeds.sql
+++
@@ -1,6 +0,0 @@
-//-----
-
-INSERT INTO employees (last_name, first_name, department, role, manager, salary)
-VALUES
-('Stephens','Scott','sales','associate','Martin Brodeur',20000); 
-('Doan', 'Shane', 'sales', 'associate', 'Keith Tkachuk', 50000)
